52861fa3b7d811c0e243836d07b7a4cf053cb0e7
DaviGel <geloso.davide@gmail.com>
2023-11-14 16:39:34 +0100

    feat: pari e dispari

---
+++ pariedispari/js/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// Variables
+
+let scelta = '';
+let numero = 0;
+let numeroPC = 0;
+let somma = 0;
+
+// Functions
+
+function randomicNumber() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
+function evenOdd(somma) {
+  if (somma % 2 === 0) {
+    return 'pari';
+  } else {
+    return 'dispari';
+  }
+}
+
+// Code
+
+while (scelta !== 'pari' && scelta !== 'dispari') {
+  scelta = prompt(
+    `Scegli tra pari o dispari. Se scrivi altro continuerò a chiederti di scegliere finché non scriverai "pari" o "dispari"`
+  );
+}
+
+while (numero < 1 || numero > 5 || isNaN(numero)) {
+  numero = Number(
+    prompt(
+      'Scegli un numero tra 1 e 5 compresi. Se scegli un numero minore di 1 o maggiore di 5 o un carattere che non sia un numero continuerò a chiederti di scegliere un numero finché non ne scriverai uno come da richiesta'
+    )
+  );
+}
+
+numeroPC = randomicNumber();
+somma = numero + numeroPC;
+
+console.log(
+  `L'utente ha scelto ${scelta}. Il numero scelto dall'utente è ${numero}. Quello scelto dal PC è ${numeroPC}. La somma tra i due numeri è ${somma}`
+);
+
+if (evenOdd(somma) === scelta) {
+  console.log(`L'utente ha vinto!`);
+} else {
+  console.log('Ha vinto il computer.');
+}
