776c1718ef54c43e79c230cb557aa44e076d0d27
DaviGel <geloso.davide@gmail.com>
2023-11-15 11:15:20 +0100

    feat: alternativa funzione palindroma

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -8,20 +8,37 @@ let stringaTemp = '';
 // Functions
 
 function palindroma(parola) {
-  let wordlength = parola.length - 1;
-  for (let i = 0; i < parola.length / 2; i++) {
-    if (parola[i] !== parola[wordlength]) {
-      return false;
+  if (parola.length === 0) {
+    return false;
+  } else {
+    // let wordlength = parola.length - 1;
+    // for (let i = 0; i < parola.length / 2; i++) {
+    //   if (parola[i] !== parola[wordlength]) {
+    //     return false;
+    //   }
+    //   wordlength--;
+    // }
+    // return true;
+    let i = 0;
+    let j = parola.length - 1;
+    while (i < j) {
+      if (parola[i] !== parola[j]) {
+        return false;
+      } else {
+        i++;
+        j--;
+      }
     }
-    wordlength--;
+    return true;
   }
-  return true;
 }
 
 // Code
 
 // Rimuovo tutti gli spazi all'interno della stringa
 stringaTemp = parola.replaceAll(' ', '');
+// Converto tutti i caratteri della stringa in minuscolo
+stringaTemp = stringaTemp.toLowerCase();
 
 if (palindroma(stringaTemp) === true) {
   console.log(`La parola o frase "${parola}" è palindroma!`);
